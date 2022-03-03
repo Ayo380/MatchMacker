591476e66d3a81bb92adc34f70680918f1c2dc9f
olugb <olugb@DESKTOP-J05LJQ8>
2022-03-03 10:54:58 -0600

    Add project files.

---
+++ MatchMaker/driver.h
@@ -0,0 +1,182 @@
+// TODO(Ayokunle Olugboyo):  This Header files contains
+// the implementation of most of the functions used in main.cpp
+// UIN: aolug2
+// Project: Personality Test
+
+
+#include <math.h>
+#include <limits>
+#include <string>
+#include <map>
+#include <set>
+#include "myrandom.h"
+
+using namespace std;
+
+constexpr double lowest_double = std::numeric_limits<double>::lowest();
+
+/* Type: Question
+ *
+ * Type representing a personality quiz question.
+ */
+struct Question {
+  string questionText;    // Text of the question
+  map<char, int> factors;  // Map from factors to +1 or -1
+  friend bool operator<(const Question &lhs, const Question &rhs) {
+    return lhs.questionText < rhs.questionText;
+  }
+  friend bool operator==(const Question &lhs, const Question &rhs) {
+    return lhs.questionText == rhs.questionText;
+  }
+  friend bool operator!=(const Question &lhs, const Question &rhs) {
+    return lhs.questionText != rhs.questionText;
+  }
+};
+
+/* Type: Person
+ *
+ * Type representing a person, used to represent people when determining
+ * who's the closest match to the user.
+ */
+struct Person {
+  string name;           // Name of the person
+  map<char, int> scores;  // Map from factors to +1 or -1
+  friend bool operator<(const Person &lhs, const Person &rhs) {
+    return lhs.name < rhs.name;
+  }
+  friend bool operator==(const Person &lhs, const Person &rhs) {
+    return lhs.name == rhs.name;
+  }
+  friend bool operator!=(const Person &lhs, const Person &rhs) {
+    return lhs.name != rhs.name;
+  }
+};
+
+/* randomElement
+ *
+ * This function selects, at random, a Question from the inputted questions set
+ * and returns the question.  Note, this function does not remove the randomly
+ * selected question from the set.
+ */
+Question randomElement(set<Question> &questions) {
+  int ind = randomInteger(0, (int)questions.size() - 1);
+  int i = 0;
+  for (auto e : questions) {
+    if (i == ind) {
+      return e;
+    }
+    i++;
+  }
+  return {};
+}
+
+/* randomQuestionFrom
+ *
+ * This function returns a Question from the question struct and
+ * and returns the question. And randomly remove the question
+ * from the set.
+ */
+Question randomQuestionFrom(set<Question> &questions) {
+  if (questions.size() == 0) {
+    throw runtime_error("set: empty");
+  }
+  Question q = randomElement(questions);
+  questions.erase(q);
+  return q;
+}
+
+/* scoresFrom
+ *
+ * This function selects, recieve a map of the users answer
+ * as an argument. Computes each answer(factor) into scores
+ * and set them to their respective factors character.
+ * This function returns map of scores with the factors characters
+ * as key to the scores(int).
+ *
+ */
+map<char, int> scoresFrom(map<Question, int> &answers) {
+  map<char, int> score;
+  int each_score;
+  for (auto &e : answers) {
+    // subtract 3 from the answer from the user
+    each_score = e.second - 3;
+    for (auto &a : e.first.factors) {
+      // multiply it with the factor
+      score[a.first] += a.second * each_score;
+      each_score = e.second - 3;
+    }
+  }
+  return score;
+}
+
+/* normalize
+ *
+ * This function normalizes, by squaring up each factor scores and
+ * taking the square root of them. And returns their normalized score
+ * as a double.
+ */
+map<char, double> normalize(map<char, int> &scores) {
+  double norm = 0;
+  map<char, double> normal;
+  // Squaring up each scores
+  for (auto a : scores) {
+    norm += (a.second * a.second);
+  }
+  norm = sqrt(norm);
+  if (norm == 0) {
+    throw runtime_error("score is empty");
+  } else {
+    for (auto a : scores) {
+      normal[a.first] = a.second / norm;
+    }
+  }
+  return normal;
+}
+
+/* cosineSimilarityOf
+ *
+ * This function selects, this function take two normalized
+ * score maps as an arguement, and calculates their similarities
+ * by multiplying thier normalized score of each facotor and returning
+ * it as a double.
+ */
+double cosineSimilarityOf(const map<char, double> &lhs,
+                          const map<char, double> &rhs) {
+  double similarity = 0;
+  for (auto e : lhs) {
+    // check if the key exist on the other side
+    if (rhs.count(e.first)) {
+      similarity += e.second * rhs.at(e.first);
+    }
+  }
+  return similarity;
+}
+
+/* mostSimilarTo
+ *
+ * This function functions returns the person with the most
+ * similarities, function uses normalize, cosineSimilarityOf to get
+ * the similarities.
+ */
+Person mostSimilarTo(map<char, int> &scores, set<Person> &people) {
+   struct Person p;
+  if (people.empty()) {
+    throw runtime_error("People is empty");
+  }
+      double temp = -999;
+     
+      double val;
+      map <char, double> people_score;
+      map<char, double> question_score = normalize(scores);
+    for (auto a : people) {
+        people_score = normalize(a.scores);
+        val = cosineSimilarityOf(question_score, people_score);
+      // Trying to get the largest similarities
+      if (val > temp) {
+         temp =  val;
+           p = a;
+      }
+    }
+  
+  return p;
+}
---
+++ MatchMaker/main.cpp
@@ -0,0 +1,155 @@
+// TODO(Ayokunle Olugboyo):  This file contains the
+// the code for the user interface.
+// UIN: aolug2
+// Project: MatchMaker.
+
+#include "driver.h"
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+// Function #1: Load_Question *Helper function*
+// Parameters: set<Question>& q and string s
+// This function get a string from Read_Question
+// parse get teach characters in their appropriate data structure
+void Load_Question(set<Question>& q, string s) {
+    int i = 0;
+    int j = 0;
+    Question questions;
+    map<char, int> factor;
+    string question;
+    // parse through until it encounters "."
+    while (s[i] != '.') {
+        if (s[i] == '.') {
+            question += s[i];
+            break;
+        }
+        question += s[i];
+        i++;
+    }
+    questions.questionText = question + '.';
+    j = i + 1;
+    string numbers;
+    // parse through until the end of the line
+    while (s[j] != '\0') {
+        // checkes for negative factors
+        if (s[j + 1] == ':' && s[j + 2] == '-') {
+            factor.emplace(s[j], (s[j + 3] - '0') * -1);
+        }
+        else if (s[j + 1] == ':') {
+            factor.emplace(s[j], s[j + 2] - '0');
+        }
+        j++;
+    }
+    // insert data of struct question into the set
+    questions.factors = factor;
+    q.insert(questions);
+}
+// Function #2: Read_Question
+// Parameters: set<Question>& p
+// This function opens the file get each line from the questions.txt file
+// then it loads it into the helper function "Load_Question".
+void Read_Question(set<Question>& q) {
+
+    ifstream openfile;
+    openfile.open("questions.txt");
+    string file_data;
+    if (openfile.is_open()) {
+        while (getline(openfile, file_data)) {
+            if (!openfile.fail()) {
+                Load_Question(q, file_data);
+            }
+        }
+    }
+    else {
+        cout << "could not open file" << endl;
+    }
+    openfile.close();
+}
+
+// Function : Menu()
+// It contains the menu for the questions
+void Menu() {
+    cout << "1. Strongly disagree" << endl;
+    cout << "2. Disagree" << endl;
+    cout << "3. Neutral" << endl;
+    cout << "4. Agree" << endl;
+    cout << "5. Strongly agree" << endl << endl;
+    cout << "Enter your answer here (1-5): ";
+}
+
+int main() {
+    // TODO:  Write your code here.  You should have lots of functions.
+    set<Question> Q;
+    set<Person> p;
+    vector <Question> questions_asked;
+    Question q;
+    vector<map<Question, int>> people_answers;
+    vector< map<char, int>> people_score;
+    map<char, int> Scores;
+    map<Question, int > answers;
+    map<char, double> normal;
+    vector<map<char, double>> norm;
+    int questions;
+    int user_answers;
+    int people;
+    int num_questions;
+    cout << "Welcome to the Personality Quiz!" << endl << endl;
+    cout << "Choose number of questions: ";
+    cin >> questions;
+    cout << "Number of people: ";
+    cin >> people;
+    Read_Question(Q);
+    num_questions = questions;
+    // Stores the questions in a set so that the same question can be asked to everyone
+    while (num_questions > 0) {
+        q = randomQuestionFrom(Q);
+        questions_asked.push_back(q);
+        num_questions--;
+    }
+    while (people > 0) {
+        while (questions > 0) {
+            for (auto e : questions_asked) {
+                char c = '"';
+                cout << endl << "How much do you agree with this statement?";
+                cout << endl << c << e.questionText << c << endl << endl;
+                Menu();
+                cin >> user_answers;
+                answers.emplace(e, user_answers);
+            }
+            questions--;
+        }
+        // saves each person answers
+        people_answers.push_back(answers);
+        people--;
+    }
+    cout << endl;
+    //compute the score for each person
+    for (auto e : people_answers) {
+        Scores = scoresFrom(e);
+        people_score.push_back(Scores);
+    }
+    // normalize each person score
+    for (auto e : people_score)
+    {
+        normal = normalize(e);
+        norm.push_back(normal);
+    }
+
+    double number; 
+    int i = 0;
+    number = cosineSimilarityOf(norm[i], norm[i + 1]);
+    cout << number;
+
+
+
+
+
+    return 0;
+}
+
